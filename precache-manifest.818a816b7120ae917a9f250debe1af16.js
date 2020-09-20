7c6982a9884f075375be53524cb032c88065a8d5
YosorIT <yosorit@gmail.com>
2020-09-20 22:16:58 +0300

    Updates

---
+++ precache-manifest.818a816b7120ae917a9f250debe1af16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a95387a29c08b27b4bef97b7e51cba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e502a4776bc0909d7d1",
+    "url": "/static/css/main.7ba102cf.chunk.css"
+  },
+  {
+    "revision": "df68e7e93caad0eff3a2",
+    "url": "/static/js/2.68cae473.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.68cae473.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e502a4776bc0909d7d1",
+    "url": "/static/js/main.f1fb584f.chunk.js"
+  },
+  {
+    "revision": "0a349441693b841c5f60",
+    "url": "/static/js/runtime-main.ef8bbad6.js"
+  }
+]);
